a15e10433ac0c9e217078d9133a326abfea85253
Stefan Stefanov <stefanov.sm@abv.bg>
2019-10-01 11:42:20 +0300

    Update example.php

--- example.php
+++ example.php
@@ -1,21 +1,20 @@
 <?php
 // Example/unit test
+
+// helper
+function show($rs) {
+    foreach ($rs as $rec) echo sprintf('%s %s'.PHP_EOL, $rec['v'], $rec['rn']);
+    echo PHP_EOL;
+}
+
 require ('SQLMethods.class.php'); // Use your preferred class loading mechanism
 $conn = new PDO (
     'pgsql:host=<host name or IP address>;port=<port>;dbname=<database name>',
     '<dbUser>', '<dbPassword>',
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => FALSE]
 );
-
 $osql = new SQLMethods('example.sql');
 $osql -> connection($conn);
-
-// -----------------------------------------------------------------------------
-// helper
-function show($rs) {
-	foreach ($rs as $rec) echo sprintf('%s %s'.PHP_EOL, $rec['v'], $rec['rn']);
-	echo PHP_EOL;
-}
 // -----------------------------------------------------------------------------
 // no parameters
 $result = $osql -> Ruffus();

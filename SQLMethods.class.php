380750f3ac64d89029a62bfc769816276d8a4c53
Stefan Stefanov <stefanov.sm@abv.bg>
2019-12-04 09:51:24 +0200

    Update SQLMethods.class.php
    Methods chaining & prepared statements caching

--- SQLMethods.class.php
+++ SQLMethods.class.php
@@ -2,119 +2,146 @@
 
 class SQLMethods
 {
-    const   PARAM_NONE = 'NONE', PARAM_NAMED = 'NAMED', PARAM_POSITIONAL = 'POSITIONAL';
-    const   QUERY_DEF_HEADER_RX = '/^--!(.*)$/i',
-            QUERY_IDENT_RX = '/^[a-z_]\w{2,31}$/i',
-            IGNORE_LINE_RX = '/(^\s*--([^!]|$))|(^\s*$)/';
-    private $conn, $qlist;
+  const PARAM_MODES_RX      = '/^(none|named|positional)$/i',
+        QUERY_DEF_HEADER_RX = '/^--!(.*)$/i',
+        QUERY_IDENT_RX      = '/^[a-z_]\w{2,31}$/i',
+        IGNORE_LINE_RX      = '/(^\s*--([^!]|$))|(^\s*$)/';
 
-    public function __construct($sql_filename, $conn = null)
+  private $conn, $qlist;
+
+  public function __construct($sql_filename = null, $conn = null)
+  {
+    $this->conn = $conn;
+    $this->qlist = (object)[];
+    if (!is_null($sql_filename))
     {
-        $this -> conn = $conn;
-        $this -> qlist = (object)[];
-        $query_object = (object)['query' => ''];
-        $query_name = null;
+      $this->import($sql_filename);
+    }
+  }
 
-        $sqlfile = @fopen($sql_filename, 'r');
-        if (!$sqlfile)
+  public static function createInstance($conn = null)
+  {
+    return new self(null, $conn);
+  }
+
+  // PDO connection setter
+  // ----------------------------------------------------
+  public function connection($conn)
+  {
+    $this->conn = $conn;
+    return $this;
+  }
+
+  // SQL file parse/import
+  // ----------------------------------------------------
+  public function import($sql_filename)
+  {
+    $query_object = (object)['query' => ''];
+    $query_name = null;
+
+    $sqlfile = @ fopen($sql_filename, 'r');
+    if (!$sqlfile)
+    {
+      throw new Exception("SQLMethods: Failed to open SQL file '{$sql_filename}' for import");
+    }
+    $linenumber = 0;
+    while (($line = fgets($sqlfile)) !== FALSE)
+    {
+      $linenumber++;
+      if (preg_match(self::IGNORE_LINE_RX, $line)) continue;
+
+      if (preg_match(self::QUERY_DEF_HEADER_RX, $line, $result))
+      {
+        $query_parameters = json_decode(trim($result[1]));
+        if
+        (
+          is_null($query_parameters) || (count((array) $query_parameters) != 2) ||
+          !isset($query_parameters->name) || !isset($query_parameters->param_mode) ||
+          !preg_match(self::QUERY_IDENT_RX, $query_parameters->name) ||
+          !preg_match(self::PARAM_MODES_RX, $query_parameters->param_mode)
+        )
         {
-            throw new Exception("SQLMethods: SQL file {$sql_filename} failed to open");
+          throw new Exception("SQLMethods: Bad query definition header, file '{$sql_filename}', line {$linenumber}");
         }
-        $linenumber = 0;
-        while (($line = fgets($sqlfile)) !== FALSE)
-        {
-            $linenumber++;
-            if (preg_match(self::IGNORE_LINE_RX, $line)) continue;
-
-            if (preg_match(self::QUERY_DEF_HEADER_RX, $line, $result))
-            {
-                $query_parameters = json_decode(trim($result[1]));
-                if
-                (
-                    is_null($query_parameters) || (count((array) $query_parameters) != 2) ||
-                    !isset($query_parameters -> name) || !isset($query_parameters -> param_mode) ||
-                    !preg_match(self::QUERY_IDENT_RX, $query_parameters -> name) ||
-                    !in_array($query_parameters -> param_mode, [self::PARAM_NONE, self::PARAM_NAMED, self::PARAM_POSITIONAL])
-                )
-                {
-                    throw new Exception("SQLMethods: Bad query definition header, file '{$sql_filename}', line {$linenumber}");
-                }
-                if (!is_null($query_name))
-                {
-                    $this -> qlist -> {strtoupper($query_name)} = $query_object;
-                    $query_object = (object)['query' => ''];
-                }
-                $query_name = $query_parameters -> name;
-                $query_object -> parameterized = $query_parameters -> param_mode;
-                continue;
-            }
-            if (!is_null($query_name))
-            {
-                $query_object -> query .= $line;
-            }
-        }
-        fclose($sqlfile);
-
         if (!is_null($query_name))
         {
-            $this -> qlist -> {strtoupper($query_name)} = $query_object;
+          $this->qlist->{strtoupper($query_name)} = $query_object;
+          $query_object = (object)['query' => ''];
         }
+        $query_name = $query_parameters->name;
+        $query_object->parameterized = $query_parameters->param_mode;
+        continue;
+      }
+      if (!is_null($query_name))
+      {
+        $query_object->query .= $line;
+      }
+    }
+    fclose($sqlfile);
+
+    if (!is_null($query_name))
+    {
+        $this->qlist->{strtoupper($query_name)} = $query_object;
+    }
+    return $this;
+  }
+
+  // Dynamic method invocation handler
+  // ----------------------------------------------------
+  public function __call($method, $arguments = null)
+  {
+    $method = strtoupper($method);
+    if (!isset($this->qlist->{$method}))
+    {
+      throw new Exception("SQLMethods: Method/query '{$method}' not defined");
     }
 
-    // PDO connection getter/setter
-    // ----------------------------------------------------
-    public function connection($conn = null)
+    switch (strtoupper($this->qlist->{$method}->parameterized))
     {
-        $current_connection = $this -> conn;
-        if (!is_null($conn))
+      case 'NONE':
+        $rs = $this->conn->query($this->qlist->{$method}->query);
+        break;
+
+      case 'NAMED':
+        if (!isset($this->qlist->{$method}->prepared))
         {
-            $this -> conn = $conn;
+          $this->qlist->{$method}->prepared = $this->conn->prepare($this->qlist->{$method}->query);
         }
-        return $current_connection;
-    }
+        $rs = $this->qlist->{$method}->prepared;
+        $rs->execute($arguments[0]);
+        break;
 
-    // Dynamic method invocation handler
-    // ----------------------------------------------------
-    public function __call($function_name, $arguments = null)
-    {
-        $function_name = strtoupper($function_name);
-        if (!isset($this -> qlist -> $function_name))
+      case 'POSITIONAL':
+        if (!isset($this->qlist->{$method}->prepared))
         {
-            throw new Exception("SQLMethods: Method (query) '{$function_name}' not defined");
+          $this->qlist->{$method}->prepared = $this->conn->prepare($this->qlist->{$method}->query);
         }
+        $rs = $this->qlist->{$method}->prepared;
+        $rs->execute($arguments);
+    }
+    return $rs;
+  }
 
-        $qdef = $this -> qlist -> $function_name;
-        switch ($qdef -> parameterized)
+  // For testing & debugging purposes
+  // ----------------------------------------------------
+  public function __toString()
+  {
+    return print_r($this->qlist, TRUE);
+  }
+
+  public static function dump_rs($rs)
+  {
+    $retvalArray = [];
+    foreach($rs as $running_record)
+    {
+      foreach($running_record as $key => $value)
+      {
+        if (is_string($key))
         {
-            case self::PARAM_NONE:
-                $rs = $this -> conn -> query($qdef->query);
-                break;
-
-            case self::PARAM_NAMED:
-                $rs = $this -> conn -> prepare($qdef->query);
-                $rs -> execute($arguments[0]);
-                break;
-
-            case self::PARAM_POSITIONAL:
-                $rs = $this -> conn -> prepare($qdef->query);
-                $rs -> execute($arguments);
+          $retvalArray[] = "{$key}: {$value}";
         }
-        return $rs;
+      }
     }
-
-    // For testing & debugging purposes
-    // ----------------------------------------------------
-    public function __toString()
-    {
-      return print_r($this -> qlist, TRUE);
-    }
-
-    public function dump_rs($rs)
-    {
-      $retArr = [];
-      foreach($rs as $rec)
-        foreach($rec as $key => $value)
-          if (is_string($key)) $retArr[] = "$key: $value".PHP_EOL;
-      return implode('', $retArr);
-    }
+    return implode(PHP_EOL, $retvalArray);
+  }
 }

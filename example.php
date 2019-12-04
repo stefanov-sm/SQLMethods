922b788cce80b4966c2eba1262620841729bb51b
Stefan Stefanov <stefanov.sm@abv.bg>
2019-12-04 09:54:18 +0200

    Methods chaining

--- example.php
+++ example.php
@@ -1,22 +1,44 @@
 <?php
-// Example/unit test
-require ('SQLMethods.class.php'); // Use your preferred class loading mechanism
+// SQLMethods example/unit test
+
+// Use your preferred class loading mechanism
+require ('SQLMethods.class.php');
+
+// Obtain a PDO connection in your preferred way
 $conn = new PDO (
-    'pgsql:host=<host name or IP address>;port=<port>;dbname=<database name>',
-    '<dbUser>', '<dbPassword>',
+    'pgsql:host=172.30.0.10;port=5432;dbname=playground',
+    'phpUser', 'Baba123Meca',
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => FALSE]
 );
-// -----------------------------------------------------------------------------
-$osql = new SQLMethods('example.sql', $conn);
 
-// no parameters
-$result = $osql -> Ruffus();
-echo $osql -> dump_rs($result).PHP_EOL;
+/* There are different ways to build SQLMethod instances
+// Use a single SQL file
+$dbgw = new SQLMethods('example.sql', $conn);
 
-// named parameters
-$result = $osql -> Buster([':hi' => 17, ':lo' => 15]);
-echo $osql -> dump_rs($result).PHP_EOL;
+// Use many SQL files
+$dbgw = new SQLMethods;
+$dbgw -> connection($conn);
+$dbgw -> import('none.sql');
+$dbgw -> import('named.sql');
+$dbgw -> import('positional.sql');
+*/
+// Use many SQL files w/ method chaining
+$dbgw = SQLMethods::createInstance($conn)
+ -> import('none.sql')
+ -> import('named.sql')
+ -> import('positional.sql');
 
-// positional parameters
-$result = $osql -> Gracie(18, 20);
-echo $osql -> dump_rs($result).PHP_EOL;
+// call a method with no parameters
+$result = $dbgw -> Ruffus();
+echo SQLMethods::dump_rs($result);
+echo PHP_EOL.PHP_EOL;
+
+// call a method with named parameters
+$result = $dbgw -> Buster([':hi' => 17, ':lo' => 15]);
+echo SQLMethods::dump_rs($result);
+echo PHP_EOL.PHP_EOL;
+
+// call a method with positional parameters
+$result = $dbgw -> Gracie(18, 20);
+echo SQLMethods::dump_rs($result);
+echo PHP_EOL.PHP_EOL;
